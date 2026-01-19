0162cebdb04b6f1387335332dbeecc0574524de8
Tulkas39 <emimiranda059@gmail.comgit config --listgit config --listgit config --global user.name Tulkas39git config --global user.email emimiranda059@gmail.com>
2026-01-19 18:10:12 -0300

    Agregando mensajes alert

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-// Array de alumnos
+let profesores = [];
+let nombreProfesor = prompt ("Ingrese el nombre del profesor:");
+profesores.push(nombreProfesor);
+
+function saludar(nombreProfesor) {
+    alert ("¡Hola," + nombreProfesor + "!¿Cómo está? Bienvenido al programa de carga de calificaciones ");
+}
+console.log(saludar(nombreProfesor));
+
 let alumnos = [];
 
 // Pedir el nombre del alumno
-let nombreAlumno = prompt("Ingrese el nombre del alumno:");
+let nombreAlumno = prompt("Ingrese el nombre del alumno a evaluar:");
 
 // Agregar el alumno al array
 alumnos.push(nombreAlumno);
@@ -26,10 +34,8 @@ for (let i = 0; i < 3; i++) {
     let calificacion = prompt("Ingrese la calificacion de " + aprendizajes[i]);
     calificaciones.push(parseFloat(calificacion));
 }
-// let calificacion = parseFloat(prompt("Ingrese la calificación de " + nombreAlumno + ":"));
 
 // función de promedio
-
 function calcularPromedio(calificaciones) {
     if (calificaciones.lenght === 0) return 0;
     let suma = 0;
@@ -45,8 +51,10 @@ console.log (promedioAlumno);
 // Verificar si está aprobado o desaprobado
 if (promedioAlumno >= 7) {
     console.log(nombreAlumno + " está APROBADO con una calificación de " + promedioAlumno);
+    alert (nombreAlumno + " está APROBADO con una calificación de " + promedioAlumno);
 } else {
     console.log(nombreAlumno + " está DESAPROBADO con una calificación de " + promedioAlumno);
+    alert (nombreAlumno + " está DESAPROBADO con una calificación de " + promedioAlumno);
 }
 
 // Mostrar el array de alumnos
